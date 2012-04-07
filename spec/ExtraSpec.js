327f227b7eeff41978bc44938bc3be12ca9f5ddd
Mark Rushakoff <mark.rushakoff@gmail.com>
2012-04-06 19:02:40 -0700

    One failing spec, one easily filtered out spec

---
+++ spec/ExtraSpec.js
@@ -0,0 +1,9 @@
+describe("Player failure", function() {
+    it("fails", function() {
+        expect(0).toBe(1);
+    });
+});
+
+describe("Something to skip", function() {
+    it("is ignored", function() {});
+});
